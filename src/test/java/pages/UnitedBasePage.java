4a5e68000a3eb9b75b4dcd340b40a78b00592286
Christina Stameson <stamesonc@gmail.com>
2023-01-31 14:51:20 -0600

    scndcommt

--- src/test/java/pages/UnitedBasePage.java
+++ src/test/java/pages/UnitedBasePage.java
@@ -1,12 +1,83 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
+
+import java.util.List;
 
 public class UnitedBasePage {
 
     public UnitedBasePage() {
         PageFactory.initElements(Driver.getDriver(), this);}
 
+
+    @FindBy(xpath = "//div[@role='tablist']/a")
+    public List<WebElement> headerTabs;
+
+    @FindBy(xpath = "(//ul[@role='tablist'])[1]/li")
+    public List<WebElement> bookTravelTabs;
+
+
+    @FindBy(id = "roundtrip")
+    public WebElement roundTripButton;
+
+    @FindBy(css = "label[for='roundtrip']")
+    public WebElement roundTripDisplay;
+
+    @FindBy(id = "oneway")
+    public WebElement oneWayButton;
+
+    @FindBy(css = "label[for='oneway']")
+    public WebElement oneWayDisplay;
+
+    @FindBy(css = "div[aria-label='Book with miles']")
+    public WebElement bookWithMilesCheckbox;
+
+    @FindBy(id = "award")
+    public WebElement bookWithMilesCheckboxSelect;
+
+    @FindBy(css = "div[aria-label='Flexible dates']")
+    public WebElement flexibleDatesCheckbox;
+
+    @FindBy(id = "flexibleDates")
+    public WebElement flexibleDatesCheckboxSelect;
+
+    @FindBy(id = "bookFlightOriginInput")
+    public WebElement fromButton;
+
+    @FindBy(css = "button[class*='clearButton']")
+    public WebElement clearFromButton;
+
+    @FindBy(id = "bookFlightDestinationInput")
+    public WebElement toButton;
+
+    @FindBy(css = "input[name='DepartDate']")
+    public WebElement departDateButton;
+
+    @FindBy(css = "button[aria-label*='Move backward']")
+    public WebElement previousMonthButton;
+
+    @FindBy(css = "td[aria-label*='February 28']")
+    public WebElement dateInput;
+
+    @FindBy(css = "button[class*='passengerButton']")
+    public WebElement passengerButton;
+
+    @FindBy(css = "button[aria-label='Substract one Adult']")
+    public WebElement addAnAdult;
+
+    @FindBy(css = "button[class*='listBoxMainButton']")
+    public WebElement selectClassButton;
+
+    @FindBy(css = "li[aria-label='Business or First']")
+    public WebElement selectBusinessOrFirstClass;
+
+    @FindBy(css = "button[aria-label='Find flights']")
+    public WebElement findFlightsButton;
+
+    @FindBy(xpath = "(//div[@class='app-components-Shopping-ColumnSortResultHeader-styles__detailHeading--2d8nC'])[2]")
+    public WebElement departureDetails;
 
 }
--- src/test/java/scripts/ValidateMainMenuTest.java
+++ src/test/java/scripts/ValidateMainMenuTest.java
@@ -20,6 +20,11 @@ public class ValidateMainMenuTest extends UnitedBase{
 
     }
 
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
     @Test(priority = 1, description = "Validate Main Menu")
     public void validateMainMenu(){
 
@@ -32,8 +37,77 @@ public class ValidateMainMenuTest extends UnitedBase{
         }
     }
 
-    @AfterMethod
-    public void tearDown(){
-        driver.quit();
+
+    @Test(priority = 2, description = "Validate Book travel menu navigation items")
+    public void validateBookTravelMenu(){
+        String[] str = {"Book", "Flight status", "Check-in", "My trips"};
+        for (int i = 0; i < str.length; i++) {
+            Assert.assertTrue(validateMainMenuPage.bookTravelTabs.get(i).isDisplayed());
+            Assert.assertEquals(validateMainMenuPage.bookTravelTabs.get(i).getText(), str[i]);
+
+        }
     }
-}
+
+
+    @Test(priority = 3, description = "Validate Round-trip and One-way radio buttons")
+    public void validateroundTripButtonAndoneWayButtonButtons(){
+        Assert.assertTrue(validateMainMenuPage.roundTripDisplay.isDisplayed());
+        Assert.assertTrue(validateMainMenuPage.roundTripButton.isEnabled());
+        Assert.assertTrue(validateMainMenuPage.roundTripButton.isSelected());
+        Assert.assertTrue(validateMainMenuPage.oneWayDisplay.isDisplayed());
+        Assert.assertTrue(validateMainMenuPage.oneWayButton.isEnabled());
+        Assert.assertFalse(validateMainMenuPage.oneWayButton.isSelected());
+        validateMainMenuPage.oneWayButton.click();
+        Assert.assertTrue(validateMainMenuPage.oneWayButton.isSelected());
+        Assert.assertFalse(validateMainMenuPage.roundTripButton.isSelected());
+
+    }
+
+
+    @Test(priority = 4, description = "Validate Book with miles and Flexible dates checkboxes")
+    public void validateBookAndFlexibleCheckboxes(){
+        Assert.assertTrue(validateMainMenuPage.bookWithMilesCheckbox.isDisplayed());
+        Assert.assertTrue(validateMainMenuPage.bookWithMilesCheckbox.isEnabled());
+        Assert.assertFalse(validateMainMenuPage.bookWithMilesCheckboxSelect.isSelected());
+
+        Assert.assertTrue(validateMainMenuPage.flexibleDatesCheckbox.isDisplayed());
+        Assert.assertTrue(validateMainMenuPage.flexibleDatesCheckbox.isEnabled());
+        Assert.assertFalse(validateMainMenuPage.flexibleDatesCheckboxSelect.isSelected());
+
+        validateMainMenuPage.bookWithMilesCheckbox.click();
+        validateMainMenuPage.flexibleDatesCheckbox.click();
+
+        Assert.assertTrue(validateMainMenuPage.bookWithMilesCheckboxSelect.isSelected());
+        Assert.assertTrue(validateMainMenuPage.flexibleDatesCheckboxSelect.isSelected());
+
+        validateMainMenuPage.bookWithMilesCheckbox.click();
+        validateMainMenuPage.flexibleDatesCheckbox.click();
+
+        Assert.assertFalse(validateMainMenuPage.bookWithMilesCheckboxSelect.isSelected());
+        Assert.assertFalse(validateMainMenuPage.flexibleDatesCheckboxSelect.isSelected());
+
+    }
+
+    @Test(priority = 5, description = "Validate One-way ticket search results from Chicago, IL, US (ORD) to Miami, FL, US (MIA)")
+    public void validateTicketSearchResults(){
+
+        validateMainMenuPage.oneWayButton.click();
+        validateMainMenuPage.fromButton.click();
+        validateMainMenuPage.clearFromButton.click();
+        validateMainMenuPage.fromButton.click();
+        validateMainMenuPage.fromButton.sendKeys("Chicago, IL, US (ORD)");
+        validateMainMenuPage.toButton.sendKeys("Miami, FL, US (MIA)");
+        validateMainMenuPage.departDateButton.click();
+        validateMainMenuPage.previousMonthButton.click();
+        validateMainMenuPage.dateInput.click();
+        validateMainMenuPage.passengerButton.click();
+        validateMainMenuPage.addAnAdult.click();
+        Waiter.pause(1);
+        validateMainMenuPage.selectClassButton.click();
+        validateMainMenuPage.selectBusinessOrFirstClass.click();
+        validateMainMenuPage.findFlightsButton.click();
+        Assert.assertEquals(validateMainMenuPage.departureDetails.getText(), "DEPART ON: February 28");
+
+
+
+}}
